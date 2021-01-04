ff8e5af8092d74eabd0e4c3590c60702a51478cb
rohit1101 <srohit1101@gmail.com>
2021-01-04 21:56:34 +0530

    Add typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetaData: {
+    title: "AudioC0re",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
