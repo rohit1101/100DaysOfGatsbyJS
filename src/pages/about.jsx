2bac7e4b3b4970beb6b3022a513a58606b53839f
rohit1101 <srohit1101@gmail.com>
2021-01-06 21:58:45 +0530

    page query with graphQL

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,22 @@
 import React from "react"
 import Container from "../components/container"
+import { graphql } from "gatsby"
 
-const About = () => {
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+const About = ({ data }) => {
   return (
     <Container>
       <div>
-        <h1>About me</h1>
+        <h1>About {data.site.siteMetadata.title}</h1>
         <p>
           I’m good enough, I’m smart enough, and gosh darn it, people like me!
         </p>
